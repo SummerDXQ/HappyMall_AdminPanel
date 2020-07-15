597705cec8f635f6227afcb157ebb6af54e1b377
XiaoqianDing <dxq3442@gmail.com>
2020-07-15 17:58:33 +1000

    PageTitle Component

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,11 +1,19 @@
 import React,{Component} from "react";
 import './index.css';
+import PageTitle from "../../component/PageTitle";
 
 class Home extends Component{
     render() {
         return(
             <div id="page-wrapper">
-                <button className="btn btn-primary">Home</button>
+                <PageTitle title="Home">
+                    <button className="btn btn-warning">test</button>
+                </PageTitle>
+                <div className="row">
+                    <div className="col-md-12">
+                        body
+                    </div>
+                </div>
             </div>
         );
     }
